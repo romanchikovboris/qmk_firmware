1cfc0940f3f702a3bb3fa8aed40f12988d704afa
bromanchikovds <b.romanchikov@datasakura.com>
2025-05-13 14:46:25 +0800

    delete useless keyboards

---
+++ keyboards/moonlander/keymaps/romanchikov_thumbpad/config.h
@@ -0,0 +1,30 @@
+/* Copyright 2020 ZSA Technology Labs, Inc <@zsa>
+ * Copyright 2020 Jack Humbert <jack.humb@gmail.com>
+ * Copyright 2020 Christopher Courtney <drashna@live.com> (@drashna)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#define ORYX_CONFIGURATOR 
+#define RGBLIGHT_ANIMATIONS
+// #define COMBO_KEYS_COUNT 40
+// #define COMBO_MAX_SIZE 3
+// #define COMBO_STACK_MAX_SIZE 3
+// #define COMBO_WAIT_TIME 100
+#define CUSTOM_SAFE_RANGE SAFE_RANGE
+#define STARTUP_SONG SONG(PREONIC_SOUND)
+
+//#define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_SOLID_COLOR
